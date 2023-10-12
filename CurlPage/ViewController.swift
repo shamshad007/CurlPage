604d997fee1a763bfb59e4888f8f1e1001a3a1f9
shamshadios <shamshad@divii.com>
2023-10-12 15:01:48 +0530

    Initial Commit

---
+++ CurlPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurlPage
+//
+//  Created by Han River on 12/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
